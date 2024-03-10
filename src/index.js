1bd50a2a5e9cd8da9f6340097b329ee1285c684b
lyj <kasean@nate.com>
2024-03-11 03:05:18 +0900

    react router dom 설치 index수정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import app from './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //npm install react-router-dom --save 라우터설치
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
