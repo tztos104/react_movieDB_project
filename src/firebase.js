73a9b05ac5a517d1a462169a0aab24d84d4092a8
lyj <kasean@nate.com>
2024-03-12 14:24:53 +0900

    firebase 구글로그인 추가

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAl6kG95VlKT09E-28V89t33OHZYYu-LuQ",
+  authDomain: "react-moviedb-project.firebaseapp.com",
+  projectId: "react-moviedb-project",
+  storageBucket: "react-moviedb-project.appspot.com",
+  messagingSenderId: "791805015746",
+  appId: "1:791805015746:web:0ef4ef169866b70b822d5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
